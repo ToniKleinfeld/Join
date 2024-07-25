a7865280d41942ea21215c3928e9a15c2b6ab0a1
Dawncreator <165510174+Dawncreator@users.noreply.github.com>
2024-07-25 16:43:31 +0200

    3rd

---
+++ script.js
@@ -0,0 +1,10 @@
+
+/**
+ * This function is used to great someone on the command line.
+ * 
+ * @param {string} name - This is the name of the PErson You want to greet.
+ */
+
+function greet(name) {
+    consolelog('Hallo' + name)
+}
