040176badac4ead83b3ab3b831948c1b6882f834
Dawncreator <165510174+Dawncreator@users.noreply.github.com>
2024-08-05 09:35:04 +0200

    git bash

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 /**
  * This function is used to great someone on the command line.
  * 
- * @param {string} name - This is the name of the PErson You want to greet.
+ * @param {string} name - This is the name of the Person You want to greet.
  */
 
 function greet(name) {
