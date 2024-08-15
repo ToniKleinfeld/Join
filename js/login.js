da605d2a09bc176602fabfc7ad0c957798b0c6dc
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-08-15 16:00:26 +0200

    set first functions for wrong input in login

--- js/login.js
+++ js/login.js
@@ -2,21 +2,13 @@
  *  This function check if the input value match with the registered user
  */
 async function checkValidLogIn(){
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
     let users = await getData("user")
     let check =  ''; 
     
-    users.forEach(element => {     
-      if (element.code == password && element.mail == email) {
-        check = true
-        user = element.account
-        loadTasksandContactsinfos();
-      }
-    });
+    checkLoginData(users);
   
     if ( check == !true) {    
-      console.error('Wrong password or email!')
+      wronguserorpassword()
     }
   }
 
@@ -28,3 +20,29 @@ function switchloginSignupWindow() {
     document.getElementById('signupbutton').classList.toggle('d-none');
     document.getElementById('signup').classList.toggle('d-none');
 }
+
+/**
+ * 
+ * @param {array} users - userarray from Firebase
+ */
+function checkLoginData(users){
+    users.forEach(element => { 
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('password').value;
+
+      if (element.code == password && element.mail == email) {
+        check = true
+        user = element.account
+        loadTasksandContactsinfos();
+      }
+    });
+}
+
+/**
+ * show red boarder and text , when wrong user / password
+ */
+function wronguserorpassword(){
+    document.getElementById('loginform').classList.add('wronglogin');
+    document.getElementById('wronglogintext').classList.remove('d-none')
+}
+
