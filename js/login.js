64f41bd7dbf0c1d5063db13b2056852ee29e8e17
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-08-20 15:31:51 +0200

    remember me function noch nicht voll angepasst

--- js/login.js
+++ js/login.js
@@ -108,3 +108,27 @@ async function guestLogIn() {
   window.location.href = "./summary.html";
 }
 
+
+function remeberMeCheck() {
+  const rmCheck = document.getElementById("remember"),
+  emailInput = document.getElementById("email");
+
+  if (localStorage.checkbox && localStorage.checkbox !== "") {
+    rmCheck.setAttribute("checked", "checked");
+    emailInput.value = localStorage.username;
+  } else {
+    rmCheck.removeAttribute("checked");
+    emailInput.value = "";
+  }
+}
+
+
+function lsRememberMe() {
+  if (rmCheck.checked && emailInput.value !== "") {
+    localStorage.username = emailInput.value;
+    localStorage.checkbox = rmCheck.value;
+  } else {
+    localStorage.username = "";
+    localStorage.checkbox = "";
+  }
+}
