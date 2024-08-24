ece8b8906ce6304f6450aff419a7aa52fe41d348
Bobi-DA <masterjb81@gmail.com>
2024-08-24 19:01:37 +0200

    board drag and drop

--- js/board copy.js
+++ js/board copy.js
@@ -4,7 +4,7 @@ const typeColors = {
 };
 
 
-const initialColors = {
+const initialColors ={
     "EM": '#1fd7c1',
     "MB": '#462f8a',
     "AM": '#ff7a00',
@@ -23,8 +23,7 @@ let initialsArray = [];
 
 
 function initBoard() {
-    // renderMainContent();
-    updateTableHTML();
+    renderMainContent();
 }
 
 function openAddTaskDialog() {
@@ -75,37 +74,37 @@ function getTypeColor(task, i) {
 
 function firstLastInitial(i) {
     let fullNames = tasks[i]['Assigned To'];  // Array von vollständigen Namen
+
     let initials = [];  // Initialen-Array für diese spezielle Gruppe von Namen
 
     fullNames.forEach(fullName => {
         let nameParts = fullName.split(" ");   // Trenne den vollen Namen in Vorname und Nachname
+
         // Hole den ersten Buchstaben von Vorname und Nachname
         let firstInitial = nameParts[0].charAt(0);
         let lastInitial = nameParts[1].charAt(0);
 
         initials.push(firstInitial + lastInitial);   // Füge Initialen in das spezielle Initialen-Array ein
-    }
-    );
+    });
 
     initialsArray.push(initials);  // Füge das spezifische Initialen-Array in das übergeordnete Array ein
     // console.log("Initialien-Arrays: ", initialsArray);  // Ausgabe des übergeordneten Initialen-Arrays
 }
 
 
-function renderCardHTML(element, i) {
-
+function renderCardHTML(i) {
     return /*html*/`
     <div id="cardContainer${i}" class="card-container" draggable="true" ondragstart="startDragging(${i})">
         <div class="card">
             <div class="frame-119">
                 <div id="labelBoardCard${i}" class="label-board-card">
                     <div id="cardTaskCategory" class="user-story">
-                        ${element.progress}
+                        ${tasks[i]['category']}
                     </div>
                 </div>
                 <div class="frame-114">
-                    <div id="cardTitle" class="title">${element.title}</div>
-                    <div id="cardDescription" class="card-description">${element.description}</div>
+                    <div id="cardTitle" class="title">${tasks[i]['title']}</div>
+                    <div id="cardDescription" class="card-description">${tasks[i]['description']}</div>
                 </div>
                 <div class="progress">
                     <div class="progressbar">
@@ -131,6 +130,41 @@ function renderCardHTML(element, i) {
 }
 
 
+
+
+
+// function fillTask() {
+//     let title = document.getElementById('enterATitle');
+//     let description = document.getElementById('enterADescription');
+//     let selectContact = document.getElementById('selectContacts');
+//     let date = document.getElementById('iputDate');
+//     let category = document.getElementById('category');
+//     let subtasks = document.getElementById('subtasks');
+
+//     document.getElementById('prioUrgendBtn').onclick = function () {
+//         console.log('Urgent button clicked');
+//     };
+
+//     document.getElementById('prioMediumBtn').onclick = function () {
+//         console.log('Medium button clicked');
+//     };
+
+//     document.getElementById('prioLowBtn').onclick = function () {
+//         console.log('Low button clicked');
+//     };
+
+
+//     console.log(title.value);
+//     console.log(selectContact.value);
+//     console.log(description.value);
+//     console.log(date.value);
+//     console.log(category.value);
+//     console.log(subtasks.value);
+
+// }
+
+
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -142,49 +176,12 @@ function startDragging(id) {
 function moveTo(category) {
     document.getElementById('category').classList('tabelle-card') = category;
 }
+// function drag(ev) {
+//     ev.dataTransfer.setData('text', ev.target.id);
+// }
 
-
-function updateTableHTML() {
-    let toDo = tasks.filter(t => t.progress == 'to do')
-    document.getElementById('tabelToDo').innerHTML = '';
-
-    for (let index = 0; index < toDo.length; index++) {
-        firstLastInitial(index);
-        const element = toDo[index];
-        document.getElementById('tabelToDo').innerHTML += renderCardHTML(element);
-
-    }
-
-
-    let inProgress = tasks.filter(t => t.progress == 'in progress')
-    document.getElementById('tableInProgress').innerHTML = '';
-
-    for (let index = 0; index < inProgress.length; index++) {
-        firstLastInitial(index);
-        const element = inProgress[index];
-        document.getElementById('tableInProgress').innerHTML += renderCardHTML(element, index);
-
-    }
-
-
-    let awaitFeedback = tasks.filter(t => t.progress == 'Await feedback')
-    document.getElementById('tableAwaitFeedback').innerHTML = '';
-
-    for (let index = 0; index < awaitFeedback.length; index++) {
-        firstLastInitial(index);
-        const element = awaitFeedback[index];
-        document.getElementById('tableAwaitFeedback').innerHTML += renderCardHTML(element, index);
-
-    }
-
-
-    let done = tasks.filter(t => t.progress == 'Done')
-    document.getElementById('tableDone').innerHTML = '';
-
-    for (let index = 0; index < done.length; index++) {
-        firstLastInitial(index);
-        const element = done[index];
-        document.getElementById('tableDone').innerHTML += renderCardHTML(element, index);
-
-    }
-}
+// function drop(ev) {
+//     ev.preventDefault();
+//     let data = ev.dataTransfer.getData('text');
+//     ev.target.appendChild(document.getElementById(data));
+// }
