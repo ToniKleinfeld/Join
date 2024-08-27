5d90f5fbe3b45a59bd4796b3a1d8334d9555a6a2
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-08-27 15:57:28 +0200

    fixed selectfield noborder

---
+++ js/add-task.js
@@ -0,0 +1,21 @@
+ let Data = {
+    "Assigned To": [
+      ""
+    ],
+    "category": "",
+    "description": "",
+    "duedate": "",
+    "prio": "Medium",
+    "subtask": [
+      {
+        "state": false,
+        "title": ""
+      }
+    ],
+    "title": "",
+    "progress": "to Do"      
+  }
+
+  function showContactslistToAssign() {
+      document.getElementById('contact-select').classList.toggle('visible');
+  }
