9d6bd371c73ffc8e82717d8e66cb7b1b5ebc79a5
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-08-29 22:18:28 +0200

    add more function for Assigned to only search missing

--- js/add-task.js
+++ js/add-task.js
@@ -18,6 +18,9 @@
 
   let assignedToArray = [];
 
+  /**
+   * Show and hide the dropdownmenu of Assigned to
+   */
   function showContactslistToAssign() {
       document.getElementById('contact-select').classList.toggle('visible');
       document.getElementById('anchorinput').classList.toggle('d-none');
@@ -25,20 +28,88 @@
       document.getElementById('anchoricon').classList.toggle('d-none');
   }
 
+  /**
+   * Inialit all function, needed when start Add-Task side
+   */
   function initTasks() {
     renderAndloadContactsToAssign()
   }
 
+  /**
+   * render the Contacts to assign to dropdown menü
+   */
   function renderAndloadContactsToAssign(){
     const list = document.getElementById('assignlist');
     list.innerHTML = '';
-    
+    renderUserAsAssign(list);
+
     for (let index = 0; index < contacts.length; index++) {
       const name = contacts[index]['name'];
-      let shortname = name.match(/\b(\w)/g).join('');
+      let shortname = shortNames(name);
       
       list.innerHTML += renderHtmlContactLi(name,shortname)
-    }    
+    }   
+  }
+
+  /**
+   * render the Contacts to assign to dropdown menü
+   * 
+   * @param {string} list - where to create the HTMLcode
+   */
+  function renderUserAsAssign(list) {
+      const usershortname = shortNames(user) 
+
+      list.innerHTML += renderHtmlContactLiUser(user,usershortname);
+  }
+
+  /**
+   * 
+   * @param {string} name - from Contacts
+   * @returns - First letter of each Word
+   */
+  function shortNames(name) {
+    return  name.match(/\b(\w)/g).join('');
+  }
+
+  /**
+   * Funktion to put ore delete the contacs to the assignedToArray if not allready in
+   * 
+   * @param {string} name - ID from chosen Contact
+   */
+  function assignedToTasK(name) {
+    const userchoise = assignedToArray.filter(assignedToArray => assignedToArray == name);
+    const checkstatus = document.getElementById(name).checked;    
+
+    if (userchoise.length == 0  && checkstatus === true) {
+      assignedToArray.push(name);
+      renderAssignedContacts();
+
+    } else if (userchoise == name && checkstatus === false) {
+      const assignedToArrayWithoutName = assignedToArray.filter(function (getname) {
+         return getname !== name;        
+      });
+    assignedToArray = assignedToArrayWithoutName;
+    renderAssignedContacts()
+    }
+  }
+
+  /**
+   * Where to render the ContactsIcon from assignedToArray
+   */
+  function renderAssignedContacts() {
+    const assignedConntacts = document.getElementById('showassignedcontacts');
+    assignedConntacts.innerHTML = '';
+
+    assignedToArray.forEach(name => {
+      shortname = shortNames(name);
+      assignedConntacts.innerHTML += renderAssignedContactshtml(shortname);
+    }); 
+  }
+
+  function renderAssignedContactshtml(shortname) {
+    return /*html*/`
+       <div class="contacticon center">${shortname}</div>
+    `
   }
 
   function renderHtmlContactLi(name,shortname) {
@@ -47,22 +118,20 @@
         <input type="checkbox" id="${name}" onchange="assignedToTasK('${name}')"/>
         <span class="checkmark"></span>
         <label for="${name}">
-          <span class="contacticon">${shortname}</span> ${name} 
+          <span class="contacticon center">${shortname}</span> ${name} 
         </label>
         </li>
     `
   }
 
-  function assignedToTasK(name) {
-    const userchoise = assignedToArray.filter(assignedToArray => assignedToArray == name);
-    const checkstatus = document.getElementById(name).checked;
-
-    if (userchoise.length == 0  && checkstatus === true) {
-      console.log('if');
-      assignedToArray.push(name);
-    } else if (userchoise == name && checkstatus === false) {
-      
-      console.log('if else');
-      /**hier splice des names aus der obrigen array */
-    }
+  function renderHtmlContactLiUser(name,shortname) {
+    return /*html*/`
+      <li class="contactslistassign">
+        <input type="checkbox" id="${name}" onchange="assignedToTasK('${name}')"/>
+        <span class="checkmark"></span>
+        <label for="${name}">
+          <span class="contacticon center">${shortname}</span> ${name} (You)
+        </label>
+        </li>
+    `
   }
