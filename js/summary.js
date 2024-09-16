d3539eef333051aa0d185ede97e0f72c69c0949e
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-09-16 11:30:07 +0200

    fixed next comming date and greetingtext by daytime

--- js/summary.js
+++ js/summary.js
@@ -43,7 +43,10 @@ function countAllTask() {
  */
 function countUrgent() {    
     const urgentcount = tasks.filter(tasks => tasks.prio === "Urgent");
-    let date =  new Date(urgentcount[0].duedate).toLocaleDateString('en-us', {month:"long", day:"numeric", year:"numeric"});
+    const nextdeadline = new Date(Math.min.apply(null, tasks.map(function(e) {
+        return new Date(e.duedate);
+      })));
+    const date =  new Date(nextdeadline).toLocaleDateString('en-us', {month:"long", day:"numeric", year:"numeric"});
        
     if (urgentcount.length === 0) {
         document.getElementById('deadline').innerHTML = '';
@@ -58,14 +61,32 @@ function countUrgent() {
  * Greet massage for the loged user or guest
  */
 function greetingUser() {
-    greeting = document.getElementById('greeting');
-    userName = document.getElementById('userlogedin');
+    const greeting = document.getElementById('greeting');
+    const userName = document.getElementById('userlogedin');
 
     if (user === 'Guest') {
-        greeting.innerHTML = 'Good morning!'
+        greeting.innerHTML = getDaytime()+'!'
     } else {
-        greeting.innerHTML = 'Good morning, ';
+        greeting.innerHTML = getDaytime()+', ';
         userName.innerHTML = `${user}`;
+    }
+}
+
+/**
+ * 
+ * @returns Greeting text by current Daytime
+ */
+function getDaytime() {
+    const time = new Date().getHours();
+    
+    if (time >= 5 &&  time <= 12) {
+        return 'Good morning'
+    } else if (time >= 12 && time <= 18){
+        return 'Good afternoon'
+    } else if (time >= 18 && time <= 22) {
+        return 'Good evening'
+    } else {
+        return 'Hello'
     }
 }
 
